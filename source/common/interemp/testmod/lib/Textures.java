e07a198c458ca8fcdf3ddd0b9738865b746519c2
internetemperor <emails@email.com>
2013-05-11 23:29:34 +1000

    cleaning house

--- source/common/interemp/testmod/lib/Textures.java
+++
@@ -1,6 +0,0 @@
-package interemp.testmod.lib;
-
-public class Textures {
-    public static final String ITEMS_PNG = "/resources/items.png";
-    public static final String BLOCKS_PNG = "/resources/blocks.png";
-}
